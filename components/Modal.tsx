77ae4d833eeece5522f4886ae89ffd3d7429d1ca
juanArias8 <juan.david@monadical.com>
2024-03-20 11:01:50 -0500

    add modal component

---
+++ components/Modal.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {Modal, StyleSheet, View} from 'react-native';
+import AppButton from './AppButton';
+
+type ModalProps = {
+  children: React.ReactNode;
+  modalVisible: boolean;
+  setModalVisible: (modalVisible: boolean) => void;
+};
+
+const AppModal = ({children, modalVisible, setModalVisible}: ModalProps) => {
+  return (
+    <View style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            {children}
+            <AppButton
+              text={'Close!'}
+              onPress={() => setModalVisible(!modalVisible)}
+            />
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+});
+
+export default AppModal;
